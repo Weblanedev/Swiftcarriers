7a3ae57c834f15d0f1f25b2d8d781df01dd6c7f8
Martins <martins.ikpe@stu.cu.edu.ng>
2024-05-23 17:13:52 +0100

    feat: change of favicon

--- env.ts
+++ env.ts
@@ -1,6 +1,6 @@
 export const EnvVars = {
   SITE_NAME: 'Swiftcarriers Haulage Services Limited',
-  OG_IMAGES_URL: 'https://next-saas-starter-ashy.vercel.app/og-images/',
+  OG_IMAGES_URL: 'https://th.bing.com/th/id/OIP.ExI2tBKDCaw28yaDgKMT_QAAAA?rs=1&pid=ImgDetMain',
   URL: 'https://next-saas-starter-ashy.vercel.app/',
   MAILCHIMP_SUBSCRIBE_URL: 'https://bstefanski.us5.list-manage.com/subscribe/post?u=66b4c22d5c726ae22da1dcb2e&id=679fb0eec9',
 };
